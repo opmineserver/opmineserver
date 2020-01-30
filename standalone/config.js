c98784046e5cdfacd72d8894cf21a5c0357bd62b
Marcos <marcos.kafok@gmail.com>
2020-01-30 19:01:10 +0100

    Upload

---
+++ standalone/config.js
@@ -0,0 +1,11 @@
+var config = {
+  "url": {
+    "configuration": "up/configuration",
+    "update": "tiles/{world}/{timestamp}",
+    "sendmessage": "up/sendmessage",
+    "login": "up/login",
+    "register": "up/register",
+    "tiles": "tiles/",
+    "markers": "tiles/"
+  }
+}
